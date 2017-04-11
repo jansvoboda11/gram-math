7b1811d34b784d56bd5ef51c90ceb1ae27fbe724
Jan Svoboda <jansvoboda11@gmail.com>
2017-04-11 14:54:15 +0200

    Compile expression only once

--- src/MathEvaluator.cpp
+++ src/MathEvaluator.cpp
@@ -21,75 +21,52 @@ double MathEvaluator::evaluate(Individual& individual) {
   }
 
   // y = x^4 + x^3 + x^2 + x
-
-  vector<double> inputs{
-      -1.0,
-      -0.9,
-      -0.8,
-      -0.7,
-      -0.6,
-      -0.5,
-      -0.4,
-      -0.3,
-      -0.2,
-      -0.1,
-       0.0,
-       0.1,
-       0.2,
-       0.3,
-       0.4,
-       0.5,
-       0.6,
-       0.7,
-       0.8,
-       0.9,
-       1.0,
-  };
-
-  vector<double> correctResults{
-      0.0000,
-      -0.1629,
-      -0.2624,
-      -0.3129,
-      -0.3264,
-      -0.3125,
-      -0.2784,
-      -0.2289,
-      -0.1664,
-      -0.0909,
-       0.0000,
-       0.1111,
-       0.2496,
-       0.4251,
-       0.6496,
-       0.9375,
-       1.3056,
-       1.7731,
-       2.3616,
-       3.0951,
-       4.0000,
+  vector<pair<double, double>> inputsOutputs{
+      {-1.0,  0.0000},
+      {-0.9, -0.1629},
+      {-0.8, -0.2624},
+      {-0.7, -0.3129},
+      {-0.6, -0.3264},
+      {-0.5, -0.3125},
+      {-0.4, -0.2784},
+      {-0.3, -0.2289},
+      {-0.2, -0.1664},
+      {-0.1, -0.0909},
+      { 0.0,  0.0000},
+      { 0.1,  0.1111},
+      { 0.2,  0.2496},
+      { 0.3,  0.4251},
+      { 0.4,  0.6496},
+      { 0.5,  0.9375},
+      { 0.6,  1.3056},
+      { 0.7,  1.7731},
+      { 0.8,  2.3616},
+      { 0.9,  3.0951},
+      { 1.0,  4.0000},
   };
 
   fitness = 0.0;
 
-  for (unsigned long i = 0; i < inputs.size(); i++) {
-    te_variable variables[] = {{"x", &inputs[i]}};
+  double& x = inputsOutputs[0].first;
+  te_variable variables[] = {{"x", &x}};
 
-    int error;
+  int error;
+  te_expr* expression = te_compile(program.c_str(), variables, 2, &error);
 
-    te_expr* expression = te_compile(program.c_str(), variables, 2, &error);
+  if (!expression) {
+    return 1000;
+  }
 
-    if (!expression) {
-      return 1000;
-    }
+  for (auto& inputOutput : inputsOutputs) {
+    x = inputOutput.first;
 
     double result = te_eval(expression);
-    te_free(expression);
-
-    double diff = correctResults[i] - result;
+    double diff = inputOutput.second - result;
 
     fitness += diff * diff;
   }
+
+  te_free(expression);
 
   return fitness;
 }
